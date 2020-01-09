54a65f282b78cf3ff9f32f78d9ec90881f2f2104
nithiya <nithiya.rajamanickam@cognizant.com>
2020-01-09 09:51:33 +0530

    error induction

--- dao/src/main/java/com/cts/Dao.java
+++ dao/src/main/java/com/cts/Dao.java
@@ -6,7 +6,10 @@ package com.cts;
 public class Dao {
 
     public int daoSum(int a, int b) {
-        return a + b;  
+    	System.out.println(String.format("The value of my integer is %d", "Hello World")); // Noncompliant; an 'int' is
+    	expected rather than a String)
+    	return a + b;  
+        
  }
  
  public int daoSubtraction(int a, int b) {
